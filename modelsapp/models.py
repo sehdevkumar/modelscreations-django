6c94737e606368a532beeece1dd3799069daa7b5
sehdev <sehdev@atai.ai>
2023-07-06 17:25:43 +0530

    second commits

--- modelsapp/models.py
+++ modelsapp/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 # Create your models here.
@@ -9,7 +10,9 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100000,default='')
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=True,blank=True)
     
     def __str__(self):
         return self.title
